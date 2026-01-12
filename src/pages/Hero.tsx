46b5530edbebfa3889141910f58eb04dea0e9df7
Coderofcoders70 <Lakshayas918@gmail.com>
2026-01-12 20:10:43 +0530

    Updated resume download button

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,11 +1,11 @@
-import Card from "../components/Card";
+﻿import Card from "../components/Card";
 import { type Variants } from "framer-motion";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const HERO_CONFIG = {
     typeWriterPhrases: ["Lakshaya Sharma", "Full Stack Developer", "Frontend Developer", "Backend Developer", "AI-Enthusiast"],
-    resumeLink: "/my-Resume/Lakshaya_Sharma_Resume.pdf.pdf",
+    resumeLink: "/my-Resume/Lakshaya_Sharma_Resume.pdf",
     profileImg: "/my-photo.png",
 };
 
